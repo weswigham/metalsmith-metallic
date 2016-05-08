c0439017acf5a6b354f74b1b4d4af38924231426
Almir Filho <almirflorenciofilho@gmail.com>
2016-05-08 17:29:11 -0300

    Set files contents as string Buffers

--- test/index.js
+++ test/index.js
@@ -6,6 +6,16 @@ var fs = require('fs');
 
 
 describe('metalsmith-metallic', function(){
+  it('should set file contents as a buffer', function(done){
+    Metalsmith('test/fixture')
+      .use(metallic())
+      .build(function(err, files){
+        if (err) { return done(err); }
+        assert.strictEqual(files['index.md'].contents instanceof Buffer, true);
+        done();
+      });
+  });
+
   it('should highlight code blocks in markdown files', function(done){
     fs.readFile('test/fixture/expected/index.md', 'utf8', function (err,data) {
       if (err) {
@@ -15,12 +25,12 @@ describe('metalsmith-metallic', function(){
         .use(metallic())
         .build(function(err, files){
           if (err) { return done(err); }
-          assert.equal(data.toString(), files['index.md'].contents);
+          assert.equal(data.toString(), files['index.md'].contents.toString());
           done();
         });
     });
   });
-    
+
   it('should parse code blocks with the no-highlight language in markdown files', function(done){
     fs.readFile('test/fixture/expected/nohighlight.md', 'utf8', function (err,data) {
       if (err) {
@@ -30,12 +40,12 @@ describe('metalsmith-metallic', function(){
         .use(metallic())
         .build(function(err, files){
           if (err) { return done(err); }
-          assert.equal(data.toString(), files['nohighlight.md'].contents);
+          assert.equal(data.toString(), files['nohighlight.md'].contents.toString());
           done();
         });
     });
   });
-    
+
   it('should highlight specific languages as it does in the browser', function(done){
     fs.readFile('test/fixture/expected/csharp.md', 'utf8', function (err,data) {
       if (err) {
@@ -45,9 +55,9 @@ describe('metalsmith-metallic', function(){
         .use(metallic())
         .build(function(err, files){
           if (err) { return done(err); }
-          assert.equal(data.toString(), files['csharp.md'].contents);
+          assert.equal(data.toString(), files['csharp.md'].contents.toString());
           done();
         });
     });
-  });    
+  });
 });
