574c266fd3a4a7d4f2325c26470242d6d9477352
Wesley Wigham <wwigham@gmail.com>
2016-10-18 04:17:24 -0400

    Fix regex to match only lines starting with whitespace (#10)
    Adds test; also updates travis to no longer test on unsupported node versions, and removes the test build output from the repository (and adds it to gitignore)

---
+++ test/fixture/expected/embedded-markers.md
@@ -0,0 +1,8 @@
+The following syntax should work using this plugin:
+
+<pre><code class="hljs javascript"><span class="hljs-keyword">var</span> myCodeBlock = <span class="hljs-string">'```'</span> +
+  <span class="hljs-string">'some-code'</span> +
+  <span class="hljs-string">'```'</span>;
+done();</code></pre>
+
+And be a single js code block
--- test/index.js
+++ test/index.js
@@ -60,4 +60,19 @@ describe('metalsmith-metallic', function(){
         });
     });
   });
+  
+  it('should handle embedded fences correctly', function(done){
+    fs.readFile('test/fixture/expected/embedded-markers.md', 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err)
+      }
+      Metalsmith('test/fixture')
+        .use(metallic())
+        .build(function(err, files){
+          if (err) { return done(err); }
+          assert.equal(files['embedded-markers.md'].contents.toString(), data.toString());
+          done();
+        });
+    });
+  });
 });
